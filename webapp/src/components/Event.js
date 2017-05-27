af014bf5487b431284a3f8de5ddfc8fb240000ec
Madincea Vasile <vmadincea@gmail.com>
2017-05-27 22:46:20 +0300

    webapp. add user field to events

--- webapp/src/components/Event.js
+++ webapp/src/components/Event.js
@@ -7,6 +7,7 @@ const Event = props => (
     {
       <li className="">
         <h2 className="list-group-item-heading">{props.event.title}</h2>
+        <p className="list-group-item-text text-left"><strong>User: </strong>{props.event.user}</p>
         <p className="list-group-item-text text-left"><strong>Created on: </strong>{props.event.date}</p>
         <p className="list-group-item-text text-left"><strong>Description: </strong>{props.event.description}</p>
         <p className="list-group-item-text text-left"><strong>latitude: </strong>{props.event.lat}</p>
