e7e165edf9a2dd876291fc423ac27451d45e5e34
Madincea Vasile <vmadincea@gmail.com>
2017-05-27 21:16:45 +0300

    webapp. add live events and get the locations from them. basic Wall view

--- webapp/src/actions/events.js
+++ webapp/src/actions/events.js
@@ -1,20 +1,33 @@
 import database from '../database'
 
-export const eventupdate = (event) => {
+export const getEventsFulfilled = events => {
     return {
-        type: 'Event Update',
-        event
+        type: 'GET_EVENTS_FULFILLED',
+        events
     };
+}
+
+export const getEventsLocationsFulfilled = locations => {
+    return {
+        type: 'GET_EVENTS_LOCATIONS_FULFILLED',
+        locations
+    }
 }
 
 export function getEvents() {
     return dispatch => {
-        return database.ref('/events').once('value', snap => {
-            const event = snap.val();
-            dispatch(eventupdate(event))
+        return database.ref('/events').on('value', snap => {
+            const events = snap.val();
+            dispatch(getEventsFulfilled(events))
         })
-            .catch((error) => {
-                console.log(error);
-            });
+    }
+}
+
+export function getLocations() {
+    return dispatch => {
+        return database.ref('/events_locations').on('value', snap => {
+            const locations = snap.val();
+            dispatch(getEventsLocationsFulfilled(locations))
+        })
     }
 }
--- webapp/src/reducers/events.js
+++ webapp/src/reducers/events.js
@@ -2,9 +2,17 @@ import update from 'immutability-helper'
 
 export default(state = [], payload = {}) => {
     switch (payload.type) {
-        case 'Event Update':
-            console.log('Event: ', payload.event)
-            return update(state, {$push: payload.event})
+        case 'GET_EVENTS_FULFILLED':
+            return update(state, {$set: payload.events})
+        case 'GET_EVENTS_LOCATIONS_FULFILLED':
+            const locations = payload.locations
+            let newState = Object.assign({}, state)
+            Object.keys(locations).forEach(id => {
+                const location = locations[id]
+                newState[id].lat = location.l[0]
+                newState[id].lon = location.l[1]
+            })
+            return newState
         default:
             return state
     }
