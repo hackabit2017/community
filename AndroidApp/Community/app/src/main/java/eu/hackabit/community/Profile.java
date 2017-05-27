94457a224a87cf256d73484ea36aa55980b76624
Daniel Cioata <daniel.cioata@gmail.com>
2017-05-27 17:43:53 +0300

    complete endorsements no and name on create

--- AndroidApp/Community/app/src/main/java/eu/hackabit/community/Profile.java
+++ AndroidApp/Community/app/src/main/java/eu/hackabit/community/Profile.java
@@ -1,10 +1,14 @@
 package eu.hackabit.community;
 
 import android.content.Context;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,7 +20,14 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        final ListView listview = (ListView) findViewById(R.id.listview);
+
+        final EditText name = (EditText) findViewById(R.id.editText);
+        name.setText("Eduard Enache");
+
+        final TextView endorsementsNo = (TextView) findViewById((R.id.textView4));
+        endorsementsNo.setText("341 endorsements");
+
+        final ListView listview = (ListView) findViewById(R.id.listView);
 
         final ArrayList<EventItem> list = new ArrayList<EventItem>();
         for (int i = 0; i < 10; ++i) {
