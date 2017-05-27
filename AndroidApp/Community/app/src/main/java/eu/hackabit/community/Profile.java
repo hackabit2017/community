68ac5204145c6eb687e6148be43b2890973f717a
Daniel Cioata <daniel.cioata@gmail.com>
2017-05-27 16:52:54 +0300

    populate the events list in profile activity

--- AndroidApp/Community/app/src/main/java/eu/hackabit/community/Profile.java
+++ AndroidApp/Community/app/src/main/java/eu/hackabit/community/Profile.java
@@ -1,7 +1,14 @@
 package eu.hackabit.community;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Profile extends AppCompatActivity {
 
@@ -9,5 +16,53 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        final ListView listview = (ListView) findViewById(R.id.listview);
+
+        final ArrayList<EventItem> list = new ArrayList<EventItem>();
+        for (int i = 0; i < 10; ++i) {
+            list.add(new EventItem("title " + i, "description " + i));
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
     }
+}
+
+class EventItem{
+    private String title;
+    private String description;
+
+    public EventItem(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String toString() {
+        return this.title + " " + this.description;
+    }
+}
+
+class StableArrayAdapter extends ArrayAdapter<EventItem> {
+
+    HashMap<EventItem, Integer> mIdMap = new HashMap<EventItem, Integer>();
+
+    public StableArrayAdapter(Context context, int textViewResourceId,
+                              List<EventItem> objects) {
+        super(context, textViewResourceId, objects);
+        for (int i = 0; i < objects.size(); ++i) {
+            mIdMap.put(objects.get(i), i);
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        EventItem item = getItem(position);
+        return mIdMap.get(item);
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
 }
