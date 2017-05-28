a3596bc02c91120013ce8eb7c8a6dac9295782f9
Madincea Vasile <vmadincea@gmail.com>
2017-05-28 12:10:31 +0300

    sort events on wall

--- webapp/src/components/Wall.js
+++ webapp/src/components/Wall.js
@@ -3,10 +3,26 @@ import { connect } from 'react-redux';
 
 import Event from './Event'
 
+const _sortByDate = (event1, event2) => {
+    const date1 = event1.date
+    const date2 = event2.date
+    if (date1 < date2) {
+        return -1
+    }
+    else if (date1 > date2) {
+        return 1
+    }
+    else {
+        return 0
+    }
+}
+
 const Wall = props => {
     const eventsAsObj = props.events
     const eventsKeys = Object.keys(eventsAsObj)
-    const events = eventsKeys.map(key => Object.assign(eventsAsObj[key], {id: key}))
+    const events = eventsKeys
+      .map(key => Object.assign(eventsAsObj[key], {id: key}))
+      .sort(_sortByDate)
     return (
       <ul className="list-group event-list list-unstyled">
           {
